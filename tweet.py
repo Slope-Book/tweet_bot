417c7c01d3cd4bf4eafce07f81ca6c8a50105b16
slopebook1494 <slopebook1494@gmail.com>
2017-11-25 21:56:02 +0900

    test

--- tweet.py
+++ tweet.py
@@ -3,9 +3,10 @@ from datetime import datetime
 
 import twitter
 
-api = twitter.Api(consumer_key=os.environ["consumer_key"],
+auth = twitter.Oath(consumer_key=os.environ["consumer_key"],
                   consumer_secret=os.environ["consumer_secret"],
                   consumer_token_key=os.environ["access_token_key"],
                   consumer_token_secret=os.environ["access_token_secret"]
                   )
-api.PostUpdate("system time is %s" % datetime.now())
+t = twitter.Twitter(auth=auth)
+t.statuses.update(statuses="pythonからtwitterへの投稿テストです！")
