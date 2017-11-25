d2a6a3baa3fa1c1e833f09a36523f81fb8255c3c
slopebook1494 <slopebook1494@gmail.com>
2017-11-25 22:24:31 +0900

    改修

--- index.py
+++ index.py
@@ -3,6 +3,6 @@ from bottle import route, run
 
 @route("/")
 def hello_world():
-    return("")
+    return "hello world"
 
 run(host="0.0.0.0", port=int(os.environ.get("POST", 5000)))
--- tweet.py
+++ tweet.py
@@ -1,12 +1,23 @@
+# coding: utf-8
+from requests_oauthlib import OAuth1Session
+import json
 import os
-from datetime import datetime
+import random
+import datetime
 
-import twitter
+twitter = OAuth1Session(consumer_key=os.environ["consumer_key"],
+                        consumer_secret=os.environ["consumer_secret"],
+                        consumer_token_key=os.environ["access_token_key"],
+                        consumer_token_secret=os.environ["access_token_secret"]
+                        )
+tweets = ["文言１", "文言２", "文言３"]
 
-auth = twitter.Oath(consumer_key=os.environ["consumer_key"],
-                  consumer_secret=os.environ["consumer_secret"],
-                  consumer_token_key=os.environ["access_token_key"],
-                  consumer_token_secret=os.environ["access_token_secret"]
-                  )
-t = twitter.Twitter(auth=auth)
-t.statuses.update(statuses="pythonからtwitterへの投稿テストです！")
+randomtweet = tweets[random.randrange(len(tweets))]
+
+timestamp = datetime.datetime.today() + datetime.taimedelta(hours=9)
+timestamp = str(timestamp.strftime("%Y/%m/%d%H:%M"))
+
+params = {
+    "status": randomtweet + " " + timestamp
+}
+req = twitter.post("https://api.twitter.com/1.1/statuses/update.json", params=params)
